046f1e293a12afe12e7081838aaed28a72636c1a
robotdostopif <aron.cederlund@outlook.com>
2021-04-07 20:41:49 +0200

    Added another test

---
+++ Kattis Aaah!/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kattis_Aaah_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                input.Add(line);
+                Console.WriteLine(GoToDoctor(input));
+            }
+        }
+        public static string GoToDoctor(List<string> listOfAahs)
+        {
+            if (listOfAahs[listOfAahs.Count() - 1].Count(a => a == 'a') < listOfAahs[listOfAahs.Count()].Count(a => a == 'a'))
+            {
+                return "no";
+            }
+            else
+            {
+                return "go";
+            }
+        }
+    }
+}
